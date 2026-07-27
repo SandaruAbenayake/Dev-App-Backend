6964cfef351e6d76959a31ca0d58920118149721
Sandaru Abenayake <maggonageabenayake@gmail.com>
2026-07-27 12:47:26 +0530

    adduser

--- controller.js
+++ controller.js
@@ -1,6 +1,7 @@
 const User = require("./model");
 
 const getUsers = (req, res, next) =>{
+    // Fetch all users
     user.find()
     . then(users => {
         res.status(200).json(users);
@@ -10,6 +11,20 @@ const getUsers = (req, res, next) =>{
     });
 }
 
+const addUser = (req, res, next) =>{
+    const user = new User({
+        id: req.body.id,
+        name: req.body.name
+    });
+    // Save the user to the database
+    user.save()
+    .then(() => {
+        res.status(201).json({message: "User added successfully"});
+    })
+    .catch(err => {
+        res.status(500).json({message: "Error fetching users", error: err});
+    });
+}
 
 
 
